f7ccd7323b88aedf855e99bc08ba5832177e3723
Cheng CHEN <i.cchen@outlook.com>
2020-06-03 23:53:03 +0800

    Read Directory

--- directory.c
+++ directory.c
@@ -1,21 +1,21 @@
 #include "directory.h"
 
 
-int main(void) {
-    struct dirent *dir;
-    DIR *d = opendir("./testing");
-    if (d == NULL) {
-        printf ("Cannot open directory '%s'\n", ".");
-        return 1;
-    }
-    if (d) {
-        while ((dir = readdir(d)) != NULL) {
-            if (dir->d_type == DT_REG){
-                printf("%s\n", dir->d_name);
-            }
-
-        }
-        closedir(d);
-    }
-    return(0);
-}
+//int main(void) {
+//    struct dirent *dir;
+//    DIR *d = opendir("./testing");
+//    if (d == NULL) {
+//        printf ("Cannot open directory '%s'\n", ".");
+//        return 1;
+//    }
+//    if (d) {
+//        while ((dir = readdir(d)) != NULL) {
+//            if (dir->d_type == DT_REG){
+//                printf("%s\n", dir->d_name);
+//            }
+//
+//        }
+//        closedir(d);
+//    }
+//    return(0);
+//}
