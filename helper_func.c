66cb7e5b98c5c157a0f3cda4f68cf66e4e80bc44
Cheng CHEN <i.cchen@outlook.com>
2020-06-04 14:26:05 +0800

    File Size Query

--- helper_func.c
+++ helper_func.c
@@ -15,3 +15,12 @@ void payload_len_to_uint8(const uint64_t src, uint8_t *dest) {
     }
 }
 
+void uint64_to_uint8(const uint64_t src, uint8_t *dest, int start) {
+    // Convert the length of uint64_t to uint8_t[8] and copy to the response
+    uint8_t length[8];
+    memcpy(length, &src, sizeof(uint64_t));
+    for (int i = 0; i < 8; i++) {
+        dest[start + 0] = length[i];
+    }
+}
+
