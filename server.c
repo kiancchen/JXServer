8ec64ff82c3b6c2e77a8ef27bda7c21b261a368d
Cheng CHEN <i.cchen@outlook.com>
2020-06-04 20:43:21 +0800

    Retrieve File

--- server.c
+++ server.c
@@ -385,13 +385,11 @@ void *connection_handler(void *arg) {
             FILE *f = fopen(filename, "r");
             free(filename);
             if (!f) {
-                puts("File does not exits!");
                 send_error(data->connect_fd);
                 break;
             }
             size_t sz = file_size(f);
             if (*starting >= sz) {
-                puts("Band range");
                 send_error(data->connect_fd);
                 break;
             }
