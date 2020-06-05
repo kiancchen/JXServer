34f38ff76e3bd89b3109dd1b8f4fa09c27bad3af
Cheng CHEN <i.cchen@outlook.com>
2020-06-05 21:09:18 +0800

    Retrieve File

--- server.c
+++ server.c
@@ -430,18 +430,18 @@ void *connection_handler(void *arg) {
                 memcpy(response + 9, request_payload, 20);
                 // fill the file data
                 memcpy(response + 29, payload, *len_data);
+                for (int i = 0; i < 20 + *len_data; ++i) {
+                    printf("%x ", *response + 9 + i)
+                }
                 length += HEADER_LENGTH;
             } else {
-                puts("Need compression");
                 length = 20 + *len_data;
                 // Concatenate the payloads
                 uint8_t *uncompressed_payload = malloc(sizeof(uint8_t) * length);
                 memcpy(uncompressed_payload, request_payload, 20);
                 memcpy(uncompressed_payload + 20, payload, *len_data);
                 puts("Before compression:");
-                for (int i = 0; i < length; ++i) {
-                    printf("%x ", uncompressed_payload[i]);
-                }
+
                 // Get the length of compressed payload
                 uint64_t compressed_length = get_code_length(&dict, uncompressed_payload, length);
                 compressed_length = upper_divide(compressed_length, 8) + 1;
@@ -453,10 +453,7 @@ void *connection_handler(void *arg) {
                 // get the compressed payload and copy to the response
                 uint8_t *compressed = compress(&dict, uncompressed_payload, length);
                 memcpy(response + 9, compressed, compressed_length);
-                puts("After compression:");
-                for (int i = 0; i < compressed_length; ++i) {
-                    printf("%x ", compressed[i]);
-                }
+
 
                 // the final length of the whole response
                 length = compressed_length + HEADER_LENGTH;
