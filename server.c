a815cdd933e73025f141f72a2d3d40e310e76b55
Cheng CHEN <i.cchen@outlook.com>
2020-06-06 01:52:57 +0800

    Optimise compress

--- server.c
+++ server.c
@@ -4,6 +4,8 @@ struct dict dict;
 char *dir_path;
 struct linked_list queue;
 
+void aaa(const uint8_t *payload, uint8_t **response, uint64_t *length);
+
 /**
  *
  * @param connect_fd Connection file description
@@ -136,6 +138,23 @@ void send_empty_retrieve(int connect_fd) {
     close(connect_fd);
 }
 
+void compress_response(uint8_t **response, const uint8_t *payload, uint64_t *length) {
+    uint64_t compressed_length = get_code_length(&dict, payload, *length);
+    compressed_length = upper_divide(compressed_length, 8) + 1;
+    // make the response
+    *response = malloc(sizeof(uint8_t) * (HEADER_LENGTH + compressed_length));
+    (*response)[0] = make_header(0x5, 1, 0);
+    // fill the payload length bytes
+    uint64_to_uint8((*response) + 1, htobe64(compressed_length));
+    // get the compressed payload and copy to the response
+    uint8_t *compressed = compress(&dict, payload, *length);
+    memcpy((*response) + 9, compressed, compressed_length);
+    // the final length of the whole response
+    *length = compressed_length + HEADER_LENGTH;
+    free(compressed);
+}
+
+
 void echo_handler(const struct data *data, const message *request) {
     uint8_t *response;
     uint64_t length;
@@ -191,21 +210,8 @@ void directory_list_handler(const struct data *data, const message *request) {
         memcpy(response + 9, payload, length);
         length += HEADER_LENGTH;
     } else {
+        compress_response(&response, payload, &length);
 
-        // get the compressed length of payload
-        uint64_t compressed_length = get_code_length(&dict, payload, length);
-        compressed_length = upper_divide(compressed_length, 8) + 1;
-        // make the response
-        response = malloc(sizeof(uint8_t) * (HEADER_LENGTH + compressed_length));
-        response[0] = make_header(0x3, 1, 0);
-        // fill the payload length bytes
-        uint64_to_uint8(response + 1, htobe64(compressed_length));
-        // get the compressed payload and copy to the response
-        uint8_t *compressed = compress(&dict, payload, length);
-        memcpy(response + 9, compressed, compressed_length);
-        // the final length of the whole response
-        length = compressed_length + HEADER_LENGTH;
-        free(compressed);
     }
 
     send(data->connect_fd, response, sizeof(uint8_t) * length, 0);
@@ -214,21 +220,22 @@ void directory_list_handler(const struct data *data, const message *request) {
     free(payload);
 }
 
-void compress_response(uint8_t **response, const uint8_t *payload, uint64_t *length) {
-    uint64_t compressed_length = get_code_length(&dict, payload, *length);
-    compressed_length = upper_divide(compressed_length, 8) + 1;
-    // make the response
-    *response = malloc(sizeof(uint8_t) * (HEADER_LENGTH + compressed_length));
-    (*response)[0] = make_header(0x5, 1, 0);
-    // fill the payload length bytes
-    uint64_to_uint8((*response) + 1, htobe64(compressed_length));
-    // get the compressed payload and copy to the response
-    uint8_t *compressed = compress(&dict, payload, *length);
-    memcpy((*response) + 9, compressed, compressed_length);
-    // the final length of the whole response
-    *length = compressed_length + HEADER_LENGTH;
-    free(compressed);
-}
+//void aaa(const uint8_t *payload, uint8_t **response, uint64_t *length) {// get the compressed length of payload
+//    uint64_t compressed_length = get_code_length(&dict, payload, (*length));
+//    compressed_length = upper_divide(compressed_length, 8) + 1;
+//    // make the response
+//    (*response) = malloc(sizeof(uint8_t) * (HEADER_LENGTH + compressed_length));
+//    (*response)[0] = make_header(0x3, 1, 0);
+//    // fill the payload length bytes
+//    uint64_to_uint8((*response) + 1, htobe64(compressed_length));
+//    // get the compressed payload and copy to the response
+//    uint8_t *compressed = compress(&dict, payload, (*length));
+//    memcpy((*response) + 9, compressed, compressed_length);
+//    // the final length of the whole response
+//    (*length) = compressed_length + HEADER_LENGTH;
+//    free(compressed);
+//}
+
 
 void file_size_handler(const struct data *data, const message *request, size_t sz) {
     // convert to network byte order
