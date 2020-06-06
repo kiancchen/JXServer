570ae74ca21d6515216a1e9a6bed449caf15cd4b
Cheng CHEN <i.cchen@outlook.com>
2020-06-06 18:32:27 +0800

    Optimization

--- func_handler.c
+++ func_handler.c
@@ -99,9 +99,9 @@ void compress_response(struct dict *dict, uint8_t **response, const uint8_t *pay
 
 void decompress_payload(struct dict *dict, const message *request, uint8_t **request_payload, uint64_t *length) {
     if (request->header->compressed == (unsigned) 0) {
-        *request_payload = malloc(sizeof(uint8_t) * request->length);
-        memcpy(*request_payload, request_payload, request->length);
-//        (*request_payload) = request->payload;
+//        *request_payload = malloc(sizeof(uint8_t) * request->length);
+//        memcpy(*request_payload, request_payload, request->length);
+        (*request_payload) = request->payload;
         (*length) = request->length;
     } else {
         (*request_payload) = decompress(dict, request->payload, request->length, length);
@@ -261,6 +261,7 @@ uint8_t retrieve_handler(int connect_fd, struct dict *dict, char *dir_path, stru
 void free_request(message *request) {
     free(request->header);
     free(request->payload);
+    request->payload = NULL;
     free(request);
 }
 
